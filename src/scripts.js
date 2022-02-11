cfd00dadc5c349e7bd9ea616bdacc00bc7751feb
MateusP21 <mateuspasso@outlook.com>
2022-02-11 16:33:17 -0300

    implementando select

--- src/scripts.js
+++ src/scripts.js
@@ -1,17 +1,25 @@
 const selectTypeChildren = document.getElementById('select-type').children;
+const selectBrand = document.getElementById('select-brand')
 $('.ui.dropdown').dropdown();
 
-function addItemInSelection(text, dataValue) {
+function addItemInSelection(parent,text, dataValue) {
   const item = document.createElement('div');
   item.className = 'item';
   item.innerText = text;
   item.dataset.value = dataValue;
+  parent.appendChild(item)
 }
 
 [...selectTypeChildren].forEach((children) => {
-  children.addEventListener('click', addItemInSelection);
+  children.addEventListener('click', async () => {
+    const data = await fetchVehicle(children.dataset.value)
+    const {nome,codigo} = data
+    data.forEach(({nome,codigo}) => {
+      addItemInSelection(selectBrand,nome,codigo)
+    })
+  });
 });
 
 window.onload = async () => {
-  console.log(await fetchVehicle('carros'));
+  // console.log(await fetchVehicle('carros'));
 }
