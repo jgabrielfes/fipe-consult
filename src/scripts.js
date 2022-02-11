3cd25952097a39b76f97a0428b9bfd4fe72c4840
João Gabriel <jgabrielfes@gmail.com>
2022-02-11 15:48:09 -0300

    Meio caminho andado

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,17 @@
+const selectTypeChildren = document.getElementById('select-type').children;
 $('.ui.dropdown').dropdown();
+
+function addItemInSelection(text, dataValue) {
+  const item = document.createElement('div');
+  item.className = 'item';
+  item.innerText = text;
+  item.dataset.value = dataValue;
+}
+
+[...selectTypeChildren].forEach((children) => {
+  children.addEventListener('click', addItemInSelection);
+});
+
+window.onload = async () => {
+  console.log(await fetchVehicle('carros'));
+}
