6ee3a3638af2035554de0bd497a7e88070ade85b
João Gabriel <jgabrielfes@gmail.com>
2022-02-11 17:19:31 -0300

    Pesquisa por carro feita

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,46 @@
-const selectTypeChildren = document.getElementById('select-type').children;
-const selectBrand = document.getElementById('select-brand')
-$('.ui.dropdown').dropdown();
+const selectType = document.getElementById('search-select-type');
+const selectBrand = document.getElementById('search-select-brand');
+const selectModel = document.getElementById('search-select-model');
+const selectYear = document.getElementById('search-select-year');
 
-function addItemInSelection(parent,text, dataValue) {
-  const item = document.createElement('div');
-  item.className = 'item';
+function addItemInSelection(parent, text, dataValue) {
+  const item = document.createElement('option');
   item.innerText = text;
-  item.dataset.value = dataValue;
+  item.value = dataValue;
   parent.appendChild(item)
 }
 
-[...selectTypeChildren].forEach((children) => {
-  children.addEventListener('click', async () => {
-    const data = await fetchVehicle(children.dataset.value)
-    const {nome,codigo} = data
-    data.forEach(({nome,codigo}) => {
-      addItemInSelection(selectBrand,nome,codigo)
-    })
+selectType.addEventListener('change', async ({ target }) => {
+  selectBrand.innerHTML = '';
+  selectModel.innerHTML = '';
+  selectYear.innerHTML = '';
+  const data = await fetchVehicle(target.value);
+  data.forEach(({ nome, codigo }) => {
+    addItemInSelection(selectBrand, nome, codigo)
   });
+});
+
+selectBrand.addEventListener('change', async ({ target }) => {
+  selectModel.innerHTML = '';
+  selectYear.innerHTML = '';
+  const data = await fetchVehicle(selectType.value, target.value);
+  data.modelos.forEach(({ nome, codigo }) => {
+    addItemInSelection(selectModel, nome, codigo)
+  });
+});
+
+selectModel.addEventListener('change', async ({ target }) => {
+  selectYear.innerHTML = '';
+  const data = await fetchVehicle(selectType.value, selectBrand.value, target.value);
+  data.forEach(({ nome, codigo }) => {
+    addItemInSelection(selectYear, nome, codigo)
+  });
+});
+
+selectYear.addEventListener('change', async ({ target }) => {
+  const data = await fetchVehicle(selectType.value,
+    selectBrand.value, selectModel.value, target.value);
+  console.log(data);
 });
 
 window.onload = async () => {
