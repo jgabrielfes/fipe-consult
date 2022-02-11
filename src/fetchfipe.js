eb7c385281803004e813ea53d528f0950de9fe31
MateusP21 <mateuspasso@outlook.com>
2022-02-11 14:46:59 -0300

    fazendo algumas alteracoes nas funcoes

--- src/fetchfipe.js
+++ src/fetchfipe.js
@@ -1,22 +1,48 @@
 const URL = 'https://parallelum.com.br/fipe/api/v1';
+let typeVehicle, brandCode, modelCode, yearCode;
 
-const fetchVehicles = async (type) => {
+const fetchBrands = async (type) => {
+  typeVehicle = type;
+  const response = await fetch(`${URL}/${typeVehicle}/marcas`);
+  const json = await response.json();
+  return json;
+};
+
+const fetchModels = async (brand) => {
+  brandCode = brand;
   const response = await fetch(
-    `https://parallelum.com.br/fipe/api/v1/${type}/marcas`
+    `${URL}/${typeVehicle}/marcas/${brandCode}/modelos`
   );
   const json = await response.json();
   return json;
 };
 
-const fetchModel = async (type, code, year) => {
-  const response = await fetch(`${URL}/${type}/marcas/${code}/modelos`);
+const fetchModels = async (model) => {
+  modelCode = model;
+  const response = await fetch(
+    `${URL}/${typeVehicle}/marcas/${brandCode}/modelos/${modelCode}/anos`
+  );
+  const json = await response.json();
+  return json;
+};
+
+const fetchYears = async (year) => {
+  yearCode = year;
+  const response = await fetch(
+    `${URL}/${typeVehicle}/marcas/${brandCode}/modelos/${modelCode}/anos/${yearCode}`
+  );
   const json = await response.json();
   return json;
 };
 
 window.onload = async () => {
-  const data = await fetchVehicles('carros');
-  const modelos = await fetchModel('carros', 59);
-  console.log(modelos);
-  console.log(data);
+  const veiculo = await fetchVehicles('carros');
+  const marcas = await fetchBrands(59);
+  const modelos = await fetchModels(5585);
+  const anos = await fetchYears('2012-3');
+
+  console.log('MARCAS:', marcas);
+  console.log('VEICULO:', veiculo);
+  console.log('MODELOS:', modelos);
+  console.log('ANOS:', anos);
 };
